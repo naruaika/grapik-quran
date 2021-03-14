a018e28f8eeeaf57aa9c87615466e09216215013
Naufan Rusyda Faikar <idmuslimdev@gmail.com>
2021-03-15 04:41:57 +0700

    Fixes model query for selecting ayah sequence

--- src/model.py
+++ src/model.py
@@ -87,7 +87,7 @@ class Reader:
             f'''SELECT seq from (SELECT ROW_NUMBER() OVER (ORDER BY id) seq,
             sura, aya FROM {self._selected_quran} WHERE page=(SELECT page FROM
             {self._selected_quran} WHERE sura=? AND aya=? LIMIT 1) AND
-            TYPE="ayah" GROUP BY aya) WHERE sura=? AND aya=?'''
+            TYPE="ayah" GROUP BY sura, aya) WHERE sura=? AND aya=?'''
         self._page_cursor.execute(query, (sura_no, aya_no, sura_no, aya_no))
         return self._page_cursor.fetchone()[0]
 
